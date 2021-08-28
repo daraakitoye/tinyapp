7e4bb43ba8c834780d87b22559762a9333144263
Dara Akitoye <daraakitoye1@gmail.com>
2021-08-28 23:50:15 +0000

    Adds a random string/url with generateRandomString

--- express_server.js
+++ express_server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
 
-app.set('view engine', 'ejs');
+
+const generateRandomString = () => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const randChar = alphabet[Math.floor(Math.random() * alphabet.length)];
+  const randString = Math.random().toString(16).slice(10);
+  const randShortUrl = randChar + randString;
+  return randShortUrl;
+}
+//generateRandomString();
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//------------------------------------------------------------------------>
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -16,6 +28,12 @@ app.get('/', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
 //Add a GET Route to Show the Form
 app.get("/urls/new", (req, res) => {
