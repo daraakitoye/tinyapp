2cb00bf4f89cc4e29004091592fd385596badd64
Dara Akitoye <daraakitoye1@gmail.com>
2021-09-04 23:17:34 +0000

    small refactor to randomSTring func

--- express_server.js
+++ express_server.js
@@ -16,9 +16,9 @@ app.set('view engine', 'ejs');
 
 const generateRandomString = () => {
   let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
+    result += char.charAt(Math.floor(Math.random() * char.length));
   }
   return result;
 }
