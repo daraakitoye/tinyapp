90fd2cdd39d4b873e81194c82770eef03fbfa75f
Dara Akitoye <daraakitoye1@gmail.com>
2021-09-04 22:57:31 +0000

    Remove test comments

--- helpers.js
+++ helpers.js
@@ -1,30 +1,18 @@
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 
-const checkIfEmailExists = (email, users) => {
-  for (const user in users) {
-    const savedEmail = users[user].email;
-    if (savedEmail === email) {
-      return savedEmail.id;
-    }
-  }
-  return null;
-};
+
 
 //credit for this func: From W7M3 lecture by Lovemore J
 const getUserByEmail = (email, users) => {
   for (let id in users) {
-    if (users[id].email === email) {
-      let user = users[id];
+    const user = users[id];
+    if (user.email === email) {
       return user;
     }
   }
   return undefined;
 }
 
-// const urlDatabase = {
-//   b6UTxQ: { mainURL: "https://www.tsn.ca", userID: "aJ48lW" },
-//   i3BoGr: { mainURL: "https://www.google.ca", userID: "aJ48lW" }
-// };
 
 const urlsForUser = (id, urlDatabase) => {
   let urlsObj = {};
@@ -36,20 +24,9 @@ const urlsForUser = (id, urlDatabase) => {
   return urlsObj;
 };
 
-// const obj = urlsForUser("aJ48lW", urlDatabase)
-// console.log(Object.keys(obj));
 
 
 
-const authenticateUser = (email, password, users) => {
 
-  const user = getUserByEmail(email, users);
-  if (user) {
-    if (bcrypt.compareSync(password, user.password)) {
-      return user
-    }
-  }
-  return null;
-};
 
-module.exports = { checkIfEmailExists, authenticateUser, getUserByEmail }
+module.exports = { getUserByEmail, urlsForUser }
