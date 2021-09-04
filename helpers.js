68f5bc4bcf260852f1d333496756f310a48bf11b
Dara Akitoye <daraakitoye1@gmail.com>
2021-09-04 08:44:26 +0000

    added helper functions file

---
+++ helpers.js
@@ -0,0 +1,53 @@
+const checkIfEmailExists = (email, users) => {
+  for (const user in users) {
+    const savedEmail = users[user].email;
+    if (savedEmail === email) {
+      return savedEmail.id;
+    }
+  }
+  return null;
+};
+
+//credit for this func: From W7M3 lecture by Lovemore J
+const getUserByEmail = (email, users) => {
+  for (let id in users) {
+    if (users[id].email === email) {
+      let user = users[id];
+      return user;
+    }
+  }
+  return null;
+}
+
+// const urlDatabase = {
+//   b6UTxQ: { mainURL: "https://www.tsn.ca", userID: "aJ48lW" },
+//   i3BoGr: { mainURL: "https://www.google.ca", userID: "aJ48lW" }
+// };
+
+const urlsForUser = (id, urlDatabase) => {
+  let urlsObj = {};
+  for (urls in urlDatabase) {
+    if (id === urlDatabase[urls].userID) {
+      urlsObj[urls] = urlDatabase[urls];
+    }
+  }
+  return urlsObj;
+};
+
+// const obj = urlsForUser("aJ48lW", urlDatabase)
+// console.log(Object.keys(obj));
+
+
+
+const authenticateUser = (email, password, users) => {
+
+  const user = getUserByEmail(email, users);
+  if (user) {
+    if (user.password === password) {
+      return user;
+    }
+  }
+  return null;
+};
+
+module.exports = { checkIfEmailExists, authenticateUser }
