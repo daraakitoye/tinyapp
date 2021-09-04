9954179017af06a7f5e4d1d55fde3e4d5bbe7f8d
Dara Akitoye <daraakitoye1@gmail.com>
2021-09-04 23:12:18 +0000

    Removed cookieParser

--- express_server.js
+++ express_server.js
@@ -2,13 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session');
 const { getUserByEmail, urlsForUser } = require('./helpers')
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   secret: 'ICE-KING'
